ee157ba2bafff5db1a00c16b69a9596c59dfae59
prosper-ador <prosnyengka@gmail.com>
2025-10-14 20:23:11 +0100

    Feat: Add cache definition for analytics data
    This commit adds a cache definition for analytics data to improve performance.
    
    - Added a new cache definition named 'analytics' to `db/caches.php`.
    - The cache is configured to use application mode, simple keys, and simple data.
    
    This change enables caching of analytics data to reduce database load and improve response times.

---
+++ plugins/local/gis_ai_assistant1/db/caches.php
@@ -0,0 +1,12 @@
+<?php
+// Cache definitions for local_gis_ai_assistant1.
+
+defined('MOODLE_INTERNAL') || die();
+
+$definitions = [
+    'analytics' => [
+        'mode' => cache_store::MODE_APPLICATION,
+        'simplekeys' => true,
+        'simpledata' => true,
+    ],
+];
