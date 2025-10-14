79876260d9c70d1c8eb0d34a5566960fa59bc801
prosper-ador <prosnyengka@gmail.com>
2025-10-14 10:38:32 +0100

    chore(plugin): enable strict_types and target Moodle 5.0+ Add declare(strict_types=1); set requires=2025040100; keep release and maturity; upgrade plugin version.

--- plugins/local/gis_ai_assistant1/version.php
+++ plugins/local/gis_ai_assistant1/version.php
@@ -1,10 +1,11 @@
 <?php
+declare(strict_types=1);
 // This file is part of Moodle - http://moodle.org/
 
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_gis_ai_assistant1';
-$plugin->version   = 2025101100; // YYYYMMDDXX
-$plugin->requires  = 2024050900; // Moodle 4.5+
+$plugin->version   = 2025101400; // YYYYMMDDXX
+$plugin->requires  = 2025040100; // Moodle 5.0+
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1.0';
